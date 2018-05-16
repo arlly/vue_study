9dc92cb90c23604bbfe71ef8ee131ed917699108
FuminoriArimoto <arimoto@n-di.co.jp>
2018-05-16 18:02:31 +0900

    First Commit

---
+++ my-project/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <myheader></myheader>
+
+    <p v-if="msg.length > 0">
+      {{msg}}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+    <router-view/>
+  </div>
+
+</template>
+
+<script>
+    import myheader from './components/myheader'
+
+    export default {
+        components: {
+            myheader
+        },
+        data () {
+            return {
+                msg: 'ｷﾀ━━━━(ﾟ∀ﾟ)━━━━!!　Hello World!'
+            }
+        },
+        methods: {
+            clear () {
+                this.msg = ''
+            }
+        }
+    }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project/src/components/myheader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        ここはヘッダーです。ちゃんと出てますか？？
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "myheader"
+    }
+</script>
+
+<style scoped>
+
+</style>
